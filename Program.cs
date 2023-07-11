7ac17657462ccfe64d61fa744eda833295f599f8
Bilge Bakkal <bilgebakkal94@gmail.com>
2023-07-11 23:10:22 +0300

    upp

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace MatematikselHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matematiksel ifadeyi girin:");
+            string ifade = Console.ReadLine();
+
+            double sonuc = Hesapla(ifade);
+            Console.WriteLine("Sonuç: " + sonuc);
+        }
+
+        static double Hesapla(string ifade)
+        {
+            try
+            {
+                return Convert.ToDouble(new System.Data.DataTable().Compute(ifade, null));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Geçersiz bir ifade girdiniz.");
+                return 0;
+            }
+        }
+    }
+}
+
